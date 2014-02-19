a3786848738ee03aedba9ee34d81bd37bf3c63e7
Carsten Praßni <c_pras01@uni-muenster.de>
2014-02-19 22:03:52 +0100

    Added enum Bracket

--- meeting.h
+++ meeting.h
@@ -1,10 +1,20 @@
 #ifndef MEETING_H
 #define MEETING_H
 
+namespace kilas {
+
+enum class Bracket : char {
+    U8 = 1,
+    U10 = 2,
+    U12 = 3,
+};
+
 class Meeting
 {
 public:
     Meeting();
 };
+
+} //namespace kilas
 
 #endif // MEETING_H
