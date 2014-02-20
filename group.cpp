7c525fcb32a5986dfaad93fdccd0d95a72ef967e
Carsten Praßni <c_pras01@uni-muenster.de>
2014-02-20 11:43:19 +0100

    Added functions to add teams to or remove from the group.

--- group.cpp
+++ group.cpp
@@ -2,6 +2,30 @@
 
 using namespace kilas;
 
-Group::Group()
-{
+Group::Group() {
+}
+
+std::vector<Team*> Group::getTeams() const {
+    return teams_;
+}
+
+bool Group::addTeam(Team * const team) {
+    if (!team)
+        return false;
+
+    teams_.push_back(team);
+    return true;
+}
+
+bool Group::removeTeam(Team * const team) {
+    if (!team)
+        return false;
+
+    for (unsigned int i = 0; i < teams_.size(); i++) {
+        if (teams_.at(i) == team) {
+            teams_.erase(teams_.begin() + i);
+            return true;
+        }
+    }
+    return false;
 }
--- group.h
+++ group.h
@@ -1,13 +1,47 @@
 #ifndef GROUP_H
 #define GROUP_H
 
+#include "team.h"
+
+#include <vector>
+#include <list>
+
 namespace kilas {
 
+/**
+ * @brief Each Group consists of the teams in a Competition that do their events
+ * together at the same time.
+ *
+ * Each Group is part of a Competition.
+ */
 class Group {
 public:
     Group();
-private:
 
+    /**
+     * @brief Returns the teams in this Group.
+     * @return The teams in this group.
+     */
+    std::vector<Team*> getTeams() const;
+
+    /**
+     * @brief Adds the team to this group.
+     * @param team The Team to be added to this Group.
+     * @return true if the Team was successfully added, false else.
+     */
+    bool addTeam(Team* const team);
+
+    /**
+     * @brief Removes the team from this group if possible.
+     * @param team The Team to be removed from this Group.
+     * @return true if the Team could be removed, false else.
+     */
+    bool removeTeam(Team* const team);
+private:
+    /**
+     * @brief The teams that are doing their events together.
+     */
+    std::vector<Team*> teams_;
 };
 
 } //namespace kilas
